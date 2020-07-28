a7b3b9312549e07b0dd9caaab0537166a48d0330
Kalaiselvi Thanga Durai <selvintheboss@gmail.com>
2020-07-27 22:39:07 -0700

    java pgm

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main (string[] args) {
+public class Helloworld {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
